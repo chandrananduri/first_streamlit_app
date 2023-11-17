2bfddb337092e7fcdf68504c51f3441edf684554
chandrananduri <151028621+chandrananduri@users.noreply.github.com>
2023-11-17 14:24:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header('🍌🥭Breakfast Menu')
 streamlit.text('Idli - 2')
 streamlit.text('🍜Dosa-1 and Coffee')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
