c3d8e757208dd2d6ea8947d9d65c34d827d17f3b
chandrananduri <151028621+chandrananduri@users.noreply.github.com>
2023-11-17 14:18:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ streamlit.header('🍌🥭Breakfast Menu')
 streamlit.text('Idli - 2')
 streamlit.text('🍜Dosa-1 and Coffee')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+streamlit.dataframe(my_fruit_list)
 # Display the table on the page.
