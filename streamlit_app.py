90cf73538c9bc6564b69878aa9218d398631c5dc
chandrananduri <151028621+chandrananduri@users.noreply.github.com>
2023-11-20 15:59:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 streamlit.text('Example Scenario')
-git status
 # Display the table on the page.
