795947f7fca1c4912b8dd2f22bcc4897718312b6
chandrananduri <151028621+chandrananduri@users.noreply.github.com>
2023-11-20 16:23:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('Idli - 2')
 streamlit.text('🍜Dosa-1 and Coffee')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado'])
 streamlit.dataframe(my_fruit_list)
 streamlit.text('Example Scenario... Checking...')
 # Display the table on the page.
